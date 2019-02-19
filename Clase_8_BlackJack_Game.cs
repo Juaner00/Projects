e2f5738b1c9676f319375f7756c59cdb4172efef
Juan Esteban Ramirez Perez <46961324+Juaner00@users.noreply.github.com>
2019-02-19 08:38:47 -0500

    Update Clase_8_BlackJack_Game.cs

--- Clase_8_BlackJack_Game.cs
+++ Clase_8_BlackJack_Game.cs
@@ -3,45 +3,28 @@ using System.Collections.Generic;
 					
 public class CardGame
 {
+	static bool lose = false, segirBool = true;
+	static string segir;
+	static int total = 0, card;
+
+	static  Random nextCard = new Random();
+
 	public static void Main()
 	{	
-    bool lose = false, segirBool = true;
-    string segir;
-    int total = 0, card;
-    
-    Random nextCard = new Random();
-	    
 		for (int i = 0; i < 2; i++) 
 		{		
-			card = nextCard.Next(1, 11);
-      total += card;
-    
-      Console.WriteLine("Carta: " + card);
-      Console.WriteLine("Total: " + total + "\n");
+			SigCard();
 		}
-		
+
 		while (!lose && segirBool) 
 		{		
 			Console.WriteLine("Quieres seguir s/n");
 
 			segir = Console.ReadLine();
 
-			if (segir == "s") 
-			{
-				card = nextCard.Next(1, 11);
-        total += card;
-        
-        Console.WriteLine("Carta: " + card);
-        Console.WriteLine("Total: " + total + "\n");
-			} 
-			else if (segir == "n") 
-			{
-				segirBool = false;
-			}
-			else 
-			{
-				Console.WriteLine("Escoge una opción correcta");
-			}
+			if (segir == "s") SigCard();
+			else if (segir == "n") segirBool = false;
+			else Console.WriteLine("Escoge una opción correcta");
 
 			if (total > 21) 
 			{
@@ -51,4 +34,13 @@ public class CardGame
 		}
 		Console.WriteLine("Tu puntaje total: " + total);
 	}
+
+	static void SigCard()
+	{
+	  card = nextCard.Next(1, 11);
+	  total += card;
+
+	  Console.WriteLine("Carta: " + card);
+	  Console.WriteLine("Total: " + total + "\n");
+	} 
 }
