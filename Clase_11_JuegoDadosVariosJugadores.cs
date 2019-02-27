47a96628c9b6b597b497b55ed83886f75eecd585
Juan Esteban Ramirez Perez <46961324+Juaner00@users.noreply.github.com>
2019-02-27 07:13:21 -0500

    Create Clase_11_JuegoDadosVariosJugadores.cs

---
+++ Clase_11_JuegoDadosVariosJugadores.cs
@@ -0,0 +1,44 @@
+using System;
+
+class MainClass 
+{
+  public static void Main () 
+  {
+    Random dado = new Random();
+    int dadoSig = 0, total = 0, contadorDados = 0, jugador = 0;
+    Console.Write("Cuántos jugadores van a jugar?: ");
+    int nJugadores = int.Parse(Console.ReadLine());
+
+    while(jugador < nJugadores){
+      Console.WriteLine("\nJugador " + (jugador + 1));
+      
+      while (true){
+        dadoSig = dado.Next(1, 7);
+        total += dadoSig;
+        Console.WriteLine("Dado: " + dadoSig);
+        Console.WriteLine("Total: " + total);
+
+        if (total >= 100) {
+          Console.WriteLine("Has ganado");
+          break;
+        }
+        if (dadoSig == 1) contadorDados++;
+        else contadorDados = 0;
+        if (contadorDados == 3){
+          Console.WriteLine("Has perdido porque has sacado dos 1 seguidos");
+          break;
+        }
+
+        Console.Write("Desea continuar? (s/n): ");
+        if (Console.ReadLine() == "n") break;
+      }
+
+      Console.WriteLine("Total: " + total);
+      dadoSig = 0;
+
+      if (total >= 100) break;
+      total = 0;
+      jugador++;
+    }
+  }
+}
