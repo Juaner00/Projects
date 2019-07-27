0238c92ecab78a1c7b4b23b7ed4a8b9146849268
Juan Esteban Ramirez Perez <46961324+Juaner00@users.noreply.github.com>
2019-07-27 18:25:46 -0500

    Versión más corta del ToMorseCodeTraductor

--- POO/TranslateTextToMorseCode.cs
+++ POO/TranslateTextToMorseCode.cs
@@ -5,38 +5,25 @@ class Translator
 {
   static void Main(string[] args)
   {
-    //Hacer el código pero con diccionario
     Dictionary<char, string> alphabet = new Dictionary<char, string>() { { ' ', "/" }, { 'A', ".-" }, { 'B', "-..." }, { 'C', "-.-." }, { 'D', "-.." }, { 'E', "." }, { 'F', "..-." }, { 'G', "--." }, { 'H', "...." }, { 'I', ".." }, { 'J', ".---" }, { 'K', "-.-" }, { 'L', ".-.." }, { 'M', "--" }, { 'N', "-." }, { 'O', "---" }, { 'P', ".--." }, { 'Q', "--.-" }, { 'R', ".-." }, { 'S', "..." }, { 'T', "-" }, { 'U', "..-" }, { 'V', "...-" }, { 'W', ".--" }, { 'X', "-..-" }, { 'Y', "-.--" }, { 'Z', "--.." }, { '0', "-----" }, { '1', ".----" }, { '2', "..---" }, { '3', "...--" }, { '4', "....-" }, { '5', "....." }, { '6', "-...." }, { '7', "--..." }, { '8', "---.." }, { '9', "----." }, { '.', ".-.-.-" }, { ',', "--..--" }, { '?', "..--.." }, { '!', "-.-.--" }, { '@', ".--.-." } };
 
-    //Probar que todos los carácteres estén en el alfabeto
     Console.WriteLine("Ingresa un mensaje a ser traducido");
     char[] mensaje = Console.ReadLine().ToUpper().ToCharArray();
-
-    while (true)
-    {
-      bool charMalo = false;
-
-      foreach (char mensajeChar in mensaje)
-      {
-        if (!alphabet.ContainsKey(mensajeChar))
-        {
-          Console.WriteLine("Ingresa un mensaje válido");
-          charMalo = true;
-          break;
-        }
-      }
-      if (!charMalo) break;
-
-      Console.WriteLine("Ingresa un mensaje a ser traducido");
-      mensaje = Console.ReadLine().ToUpper().ToCharArray();
-    }
-    Console.WriteLine("Mensaje Correcto");
-
-    //Cambiar los carácters por los del código morse
+    
     string mensajeTraducido = "";
-    foreach (char mensajeChar in mensaje) mensajeTraducido += alphabet[mensajeChar] + " ";
 
-    //Retornar mensaje traducido
+    foreach (char mensajeChar in mensaje) 
+    {
+      try 
+      {
+        mensajeTraducido += alphabet[mensajeChar] + " ";
+      }
+      catch (Exception)
+      {
+        Console.WriteLine(mensajeChar + " No es un carácter válido");
+      }
+    }
+
     Console.WriteLine(mensajeTraducido);
   }
 }
