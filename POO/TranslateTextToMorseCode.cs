c3771b12c6f273054582ece14f43466b186e6bb1
Juan Esteban Ramirez Perez <46961324+Juaner00@users.noreply.github.com>
2019-07-27 18:07:49 -0500

    Update TranslateTextToMorseCode.cs

--- POO/TranslateTextToMorseCode.cs
+++ POO/TranslateTextToMorseCode.cs
@@ -5,12 +5,10 @@ class Translator
 {
   static void Main(string[] args)
   {
-    
     //Hacer el código pero con diccionario
     Dictionary<char, string> alphabet = new Dictionary<char, string>() { { ' ', "/" }, { 'A', ".-" }, { 'B', "-..." }, { 'C', "-.-." }, { 'D', "-.." }, { 'E', "." }, { 'F', "..-." }, { 'G', "--." }, { 'H', "...." }, { 'I', ".." }, { 'J', ".---" }, { 'K', "-.-" }, { 'L', ".-.." }, { 'M', "--" }, { 'N', "-." }, { 'O', "---" }, { 'P', ".--." }, { 'Q', "--.-" }, { 'R', ".-." }, { 'S', "..." }, { 'T', "-" }, { 'U', "..-" }, { 'V', "...-" }, { 'W', ".--" }, { 'X', "-..-" }, { 'Y', "-.--" }, { 'Z', "--.." }, { '0', "-----" }, { '1', ".----" }, { '2', "..---" }, { '3', "...--" }, { '4', "....-" }, { '5', "....." }, { '6', "-...." }, { '7', "--..." }, { '8', "---.." }, { '9', "----." }, { '.', ".-.-.-" }, { ',', "--..--" }, { '?', "..--.." }, { '!', "-.-.--" }, { '@', ".--.-." } };
 
     //Probar que todos los carácteres estén en el alfabeto
-
     Console.WriteLine("Ingresa un mensaje a ser traducido");
     char[] mensaje = Console.ReadLine().ToUpper().ToCharArray();
 
